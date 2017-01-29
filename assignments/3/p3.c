7f09ded79c60cd9b8f95357b68cfd37a469009f2
Summerlve <dzxx0563zh@126.com>
2017-01-29 21:31:04 +0800

    some change

--- assignments/3/p3.c
+++ assignments/3/p3.c
@@ -30,4 +30,40 @@ typedef struct {
     int valueSize;
 } multitable;
 
+void VectorNew(vector *v, int elemSize)
+{
+    v->elemSize = elemSize;
+    v->logiclLength = 0;
+    v->allocatedLength = 4;
+    v->elems = malloc(v->elemSize * v->allocatedLength);
+    assert(v->elems != NULL);
+}
+
+void VectorExpand(vector *v)
+{
+    v->allocatedLength *= 2;
+    v->elems = malloc(v->elemSize * v->allocatedLength);
+    assert(v->elems != NULL);
+}
+
+int VectorLength(vector *v)
+{
+    return v->logiclLength;
+}
+
+void *VectorNth(vector *v, int index)
+{
+    return (char *)v->elems + v->elemSize * index;
+}
+
+void VectorAdd(vector *v, void *valueAddr)
+{
+    if (v->logiclLength == v->allocatedLength)
+        VectorExpand(v);
+
+    void *dest = VectorNth(v, VectorLength(v));
+    memcpy(dest, valueAddr, v->elemSize);
+    v->logiclLength++;
+}
+
 
