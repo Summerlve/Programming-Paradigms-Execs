d3ac3db2c3ed6b327337e38d4130dba496bd683f
Summerlve <dzxx0563zh@126.com>
2017-01-31 17:34:30 +0800

    some change

--- assignments/3/p3.c
+++ assignments/3/p3.c
@@ -5,7 +5,7 @@
 typedef int (*MultiTableHashFunction)(const void *keyAddr, int numBuckets);
 typedef int (*MultiTableCompareFunction)(const void *keyAddr1, const void *keyAddr2);
 typedef int (*MultiTableMapFunction)(void *keyAddr, void *valueAddr, void *auxData);
-typedef void (*HashSetMapFunction)(void *curElem);
+typedef void (*HashSetMapFunction)(void *curElemAddr);
 
 typedef struct {
     void *elems;
@@ -56,7 +56,7 @@ void *VectorNth(vector *v, int index)
     return (char *)v->elems + v->elemSize * index;
 }
 
-void VectorAdd(vector *v, void *valueAddr)
+void VectorAppend(vector *v, const void *valueAddr)
 {
     if (v->logiclLength == v->allocatedLength)
         VectorExpand(v);
@@ -66,4 +66,102 @@ void VectorAdd(vector *v, void *valueAddr)
     v->logiclLength++;
 }
 
+void HashSetNew(hashset *hs, int numBuckets, MultiTableHashFunction hash, MultiTableCompareFunction compare, int elemSize)
+{
+    hs->numBuckets = numBuckets;
+    hs->hash = hash;
+    hs->compare = compare;
+    hs->elemSize = elemSize;
+    hs->logiclLength = 0;
+    hs->allocatedLength = 4;
+    hs->elems = malloc(hs->elemSize * hs->allocatedLength);
+    assert(hs->elems != NULL);
+}
 
+void HashSetExpend(hashset *hs)
+{
+    hs->allocatedLength *= 2;
+    hs->elems = realloc(hs->elems, hs->elemSize * hs->allocatedLength);
+    assert(hs->elems != NULL);
+}
+
+int HashSetLength(hashset *hs)
+{
+    return hs->logiclLength;
+}
+
+void *HashSetNth(hashset *hs, int index)
+{
+    return (char *)hs->elems + index * hs->elemSize;
+}
+
+void HashSetAppend(hashset *hs, void *valueAddr)
+{
+    if (hs->logiclLength == hs->allocatedLength)
+        HashSetExpend(hs);
+
+    void *dest = HashSetNth(hs, HashSetLength(hs));
+    memcpy(dest, valueAddr, hs->elemSize);
+    hs->logiclLength++;
+}
+
+void HashSetMap(hashset *hs, HashSetMapFunction map)
+{
+    for (int i = 0; i < HashSetLength(hs); i++)
+    {
+        void *curElemAddr = HashSetNth(hs, i);
+        map(curElemAddr);
+    }
+}
+
+void MultiTableNew(multitable *mt, int keySizeInBytes, int valueSizeInBytes, int numBuckets, MultiTableHashFunction hash, MultiTableCompareFunction compare)
+{
+    int elemSize = keySizeInBytes + sizeof(vector);
+    HashSetNew(&mt->mappings, numBuckets, hash, compare, elemSize);
+    mt->keySize = keySizeInBytes;
+    mt->valueSize = valueSizeInBytes;
+}
+
+void MultiTableEnter(multitable *mt, const void *keyAddr, const void *valueAddr)
+{
+    hashset *hs = &mt->mappings;
+    int compare;
+    void *curElemAddr;
+
+    for (int i = 0; i < HashSetLength(hs); i++)
+    {
+        curElemAddr = HashSetNth(hs, i);
+        compare = memcmp(curElemAddr, keyAddr, mt->keySize);
+
+        if (compare == 0) break;
+    }
+
+    if (compare != 0)
+    {
+        vector v;
+        VectorNew(&v, mt->valueSize);
+        VectorAppend(&v, valueAddr);
+
+        void *value = malloc(hs->elemSize);
+        memcpy(value, keyAddr, mt->keySize);
+        memcpy((char *)value + mt->keySize, valueAddr, mt->valueSize);
+        HashSetAppend(hs, value);
+    }
+
+    if (compare == 0)
+    {
+        vector *v = (vector *)((char *)curElemAddr + mt->keySize);
+        VectorAppend(v, valueAddr);
+    }
+}
+
+
+void MultiTableMap(multitable *mt, MultiTableMapFunction map, void *auxData)
+{
+
+}
+
+int main(void)
+{
+   return 0;
+}
