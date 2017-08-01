c86e80463056051532470c8a3931817c96f83ebc
Summerlve <dzxx0563zh@126.com>
2017-08-01 16:36:08 +0800

    ass5 p1

---
+++ 13/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int i;
+    int array[4];
+    for (i = 0; i <= 4; i++)
+    {
+        array[i] = 0;
+    }
+
+    return 0;
+}
---
+++ 13/main_2.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int array[4];
+    int i;
+    for (i = 0; i <= 4; i++)
+    {
+        array[i] -= 4;
+    }
+
+    return 0;
+}
