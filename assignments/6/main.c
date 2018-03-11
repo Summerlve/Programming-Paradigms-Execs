437faad3ff083bafb247ccda2d989be2e0bc1909
Summerlve <dzxx0563zh@126.com>
2018-03-11 22:10:02 +0800

    ass 6

--- assignments/6/main.c
+++ assignments/6/main.c
@@ -7,6 +7,8 @@
 #define NUM_STUDENTS 115
 #define NUM_MACHINES 20
 
+
+
 void *TA(void *args)
 {
     return NULL;
@@ -14,32 +16,42 @@ void *TA(void *args)
 
 void *Student(void *args)
 {
+
     return NULL;
+}
+
+void SetSomeSemaphore(void)
+{
+
 }
 
 static int Examine(void)
 {
-    return 1;
+    int min = 0;
+    int max = 10;
+    int value = rand() % (max + 1 - min) + min;
+    return value;
 }
 
 static void ReadEmail(void)
 {
-
+    printf("TA is reading email\n");
 }
 
 static void Debug(void)
 {
-
+    printf("Student is debuging\n");
 }
 
 static void Rejoice(void)
 {
-
+    printf("Student rejoice\n");
 }
 
 int main(int argc, char **argv)
 {
     InitThreadPackage(false);
+    srand(time(NULL)); // random seed init, should only be called once.
     for(int i = 0; i < NUM_TAS; i++) ThreadNew("TA", TA, 1, i);
     for(int i = 0; i < NUM_STUDENTS; i++) ThreadNew("Student", Student, 0);
     RunAllThreads();
