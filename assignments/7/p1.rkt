8842c5d212accad0fb1a8a02c1d432beb044b6d7
Summerlve <dzxx0563zh@126.com>
2018-10-03 22:23:48 +0800

    some change

---
+++ assignments/7/p1.rkt
@@ -0,0 +1,15 @@
+#lang racket
+(define (k-subsets set k)
+    (cond
+        [(= k 0) '(())]
+        [(< (length set) k) '()]
+        [(cdr set) => (lambda (subset)
+                        (if (null? subset) '(())
+                            (let ([kk-subsets-rest (k-subsets subset (- k 1))])
+                                (append kk-subsets-rest
+                                    (map
+                                        (lambda (kk-subsets-rest-item) 
+                                            (cons (car set) kk-subsets-rest-item)))))))]))
+
+(println
+    (k-subsets '(1 2 3 4) 2))
