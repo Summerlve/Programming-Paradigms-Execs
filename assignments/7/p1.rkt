dbab6fee037eeac5472c0bc912fb6ea24043a616
Summerlve <dzxx0563zh@126.com>
2018-10-03 22:30:23 +0800

    resolving ass 7 p1

--- assignments/7/p1.rkt
+++ assignments/7/p1.rkt
@@ -9,7 +9,7 @@
                                 (append kk-subsets-rest
                                     (map
                                         (lambda (kk-subsets-rest-item) 
-                                            (cons (car set) kk-subsets-rest-item)))))))]))
+                                            (cons (car set) kk-subsets-rest-item)) kk-subsets-rest)))))]))
 
 (println
     (k-subsets '(1 2 3 4) 2))
