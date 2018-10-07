dd47d6ca7c6bc982905562d8eff73cd70541fe44
Summerlve <dzxx0563zh@126.com>
2018-10-07 21:11:44 +0800

    p1.rkt completed

--- assignments/7/p1.rkt
+++ assignments/7/p1.rkt
@@ -3,13 +3,24 @@
     (cond
         [(= k 0) '(())]
         [(< (length set) k) '()]
-        [(cdr set) => (lambda (subset)
-                        (if (null? subset) '(())
-                            (let ([kk-subsets-rest (k-subsets subset (- k 1))])
-                                (append kk-subsets-rest
-                                    (map
-                                        (lambda (kk-subsets-rest-item) 
-                                            (cons (car set) kk-subsets-rest-item)) kk-subsets-rest)))))]))
+        [(cdr set) => 
+            (lambda (subset)
+                (if (null? subset) (list set)
+                    (let ([k-subsets-rest (k-subsets subset k)]
+                          [kk-subsets-rest (k-subsets subset (- k 1))])
+                        (append k-subsets-rest
+                            (map
+                                (lambda (kk-subsets-rest-item) 
+                                    (cons (car set) kk-subsets-rest-item)) kk-subsets-rest)))))]))
 
 (println
     (k-subsets '(1 2 3 4) 2))
+(println
+    (k-subsets '(1 2 3 4) 4))
+(println
+    (k-subsets '(1 2 3 4 5 6) 1))
+(println
+    (k-subsets '(#\a #\b #\c #\d) 0))
+(println
+    (k-subsets '(#\a #\b #\d #\d) 5))
+    
