3902a70fe877b6e8de433f5bdbb8688d55973f71
Summerlve <dzxx0563zh@126.com>
2018-12-27 21:03:43 +0800

    fuck p2.b

--- assignments/7/p2.b.rkt
+++ assignments/7/p2.b.rkt
@@ -1,3 +1,20 @@
 #lang racket
+
 (define (up-down-permute num-list)
-    )
+    (if (null? num-list) '(())
+        (apply append 
+            (map (lambda (elem)
+                (map (lambda (permutation)
+                    (let ([consed-permutation (cons elem permutation)])
+                        (if (= 1 (length consed-permutation)) consed-permutation
+                            (let* ([is-num-list-even (even? (length num-list))]
+                                [is-consed-permutation-even (even? (length consed-permutation))]
+                                [predicate-function
+                                    (if is-num-list-even (if is-consed-permutation-even < >)
+                                        (if is-consed-permutation-even > <))])
+                                           (if (apply predicate-function (take consed-permutation 2)) consed-permutation '())))))
+                (up-down-permute (remove elem num-list))))
+            num-list))))
+
+(println
+    (up-down-permute '(1 2 3)))
