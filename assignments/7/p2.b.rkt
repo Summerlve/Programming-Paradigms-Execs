94c54d5dada337f334cec8f55f19ee369cab2ad9
Summerlve <dzxx0563zh@126.com>
2019-01-27 22:42:38 +0800

    restart for tribble star project

--- assignments/7/p2.b.rkt
+++ assignments/7/p2.b.rkt
@@ -1,20 +1,21 @@
 #lang racket
 
 (define (up-down-permute num-list)
-    (if (null? num-list) '(())
+  (letrec ([is-num-list-even (even? (length num-list))]
+           [fn (lambda (list)
+                 (if (null? list) '(())
         (apply append 
             (map (lambda (elem)
                 (map (lambda (permutation)
                     (let ([consed-permutation (cons elem permutation)])
                         (if (= 1 (length consed-permutation)) consed-permutation
-                            (let* ([is-num-list-even (even? (length num-list))]
-                                [is-consed-permutation-even (even? (length consed-permutation))]
-                                [predicate-function
-                                    (if is-num-list-even (if is-consed-permutation-even < >)
+                            (let* ([is-consed-permutation-even (even? (length consed-permutation))]
+                                   [predicate-function(if is-num-list-even (if is-consed-permutation-even < >)
                                         (if is-consed-permutation-even > <))])
                                            (if (apply predicate-function (take consed-permutation 2)) consed-permutation '())))))
-                (up-down-permute (remove elem num-list))))
-            num-list))))
+                (fn (remove elem list))))
+            list))))])
+    (fn num-list)))
 
 (println
     (up-down-permute '(1 2 3)))
