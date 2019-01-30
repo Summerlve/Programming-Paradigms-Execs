d6ad91c8cb38e078d4f823baab230cd7659ab19e
Summerlve <dzxx0563zh@126.com>
2019-01-30 18:49:34 +0800

    my top

--- assignments/7/p2.b.rkt
+++ assignments/7/p2.b.rkt
@@ -1,21 +1,24 @@
 #lang racket
 
 (define (up-down-permute num-list)
-  (letrec ([is-num-list-even (even? (length num-list))]
-           [fn (lambda (list)
-                 (if (null? list) '(())
-        (apply append 
-            (map (lambda (elem)
-                (map (lambda (permutation)
-                    (let ([consed-permutation (cons elem permutation)])
-                        (if (= 1 (length consed-permutation)) consed-permutation
-                            (let* ([is-consed-permutation-even (even? (length consed-permutation))]
-                                   [predicate-function(if is-num-list-even (if is-consed-permutation-even < >)
-                                        (if is-consed-permutation-even > <))])
-                                           (if (apply predicate-function (take consed-permutation 2)) consed-permutation '())))))
-                (fn (remove elem list))))
-            list))))])
-    (fn num-list)))
+    (letrec ([is-num-list-even (even? (length num-list))]
+             [fn (lambda (list)
+                (if (null? list) '(())
+                (apply append 
+                    (map (lambda (elem)
+                        (map (lambda (permutation)
+                            (let ([consed-permutation (cons elem permutation)])
+                                (if (= 1 (length consed-permutation)) consed-permutation
+                                    (let* ([is-consed-permutation-even (even? (length consed-permutation))]
+                                        [predicate-function(if is-num-list-even (if is-consed-permutation-even < >)
+                                                (if is-consed-permutation-even > <))])
+                                                (if (apply predicate-function (take consed-permutation 2)) consed-permutation '())))))
+                        (fn (remove elem list))))
+                    list)
+                )))])
+    (filter (lambda (permutation) (if (= (length permutation) (length num-list)) #t #f)) (fn num-list))))
 
 (println
     (up-down-permute '(1 2 3)))
+(println
+    (up-down-permute '(1 2 3 4)))
