b148699f215ba4a409895bdcfcbaeaa416df20b4
Summerlve <dzxx0563zh@126.com>
2019-03-07 12:49:03 +0800

    final p5.b

--- assignments/final_execs/p5.rkt
+++ assignments/final_execs/p5.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
+; a)
 (define (marry singles)
     (cond 
-        [(= (length singles) 0) '()]
-        [(= (length singles) 1) (list (car singles))]
+        [(<= (length singles) 1) singles]
         [else (cons (take singles 2) (marry (list-tail singles 2)))]))
 
 (println 
@@ -17,5 +17,22 @@
 (println
     (marry '("INFP")))
 
+; b)
 (define (map-everywhere func structure)
-    )
+    (if (list? structure) 
+        (if (= (length structure) 0) structure
+            (cons
+                (map-everywhere func (car structure))
+                (map-everywhere func (cdr structure))))
+        (func structure)))
+
+(println
+    (map-everywhere (lambda (x) (+ x 1)) '(1 (2 (3) 4 (5 (6))) (8 9)))) 
+(println
+    (map-everywhere list '("a" ("b" ("c" ("d" "e" "f" "g"))))))
+(define (factorial index) 
+    (cond 
+        [(= index 1) 1]
+        [else (* index (factorial (- index 1)))]))
+(println
+    (map-everywhere factorial '(4 ((5)) (6 (7)))))
