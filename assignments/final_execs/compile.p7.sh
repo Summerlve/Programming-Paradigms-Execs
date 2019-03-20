d2e38900a9cdee7cc0f0f9425012a557091eb399
Summerlve <dzxx0563zh@126.com>
2019-03-20 11:27:04 +0800

    p7

---
+++ assignments/final_execs/compile.p7.sh
@@ -0,0 +1,1 @@
+gcc p7.c ../../16/thread_107/thread_107.c -o a.out -w -g -l pthread
--- assignments/final_execs/p7.c
+++ assignments/final_execs/p7.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../../16/thread_107/thread_107.h"
+#define __APPLE__
+
+int DownloadMediaFile(const char *server, const char *file)
+{
+    // TODO http/https request to server/file using c.
+
+    // TODO count size of downloaded content in bytes. 
+
+    return 0;
+}
+
+int DownloadMediaLibrary(const char *server, const char *files[], int numFiles)
+{
+    int files_size_sum = 0;
+    Semaphore max_connections = SemaphoreNew("max_connections", 12);
+    Semaphore files = SemaphoreNew("files", 0);
+
+    for (int i = 0; i < numFiles; i++)
+    {
+        ThreadNew("DownloadMediaFile", DownloadMediaFile, 2, server, files[i]);
+    }
+
+    for (int i = 0; i < numFiles; i++)
+    {
+        SemaphoreWait(files);
+    }
+
+    return files_size_sum;
+}
 
 int main(int argc, char **argv)
 {
+    InitThreadPackage(false);
+
+    char server[] = "www.google.com";
+    const char *files[] = {
+        "file1",
+        "file2"
+    };
+
+    RunAllThreads(); 
+    int files_size_sum = DownloadMediaFile(server, files); 
+
     return 0;
 }
