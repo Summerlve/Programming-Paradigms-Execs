5427ac1ee47c6223a28034a745fda0dacf01d1f1
Summerlve <dzxx0563zh@126.com>
2019-03-25 20:48:19 +0800

    add libcurl usage

--- assignments/final_execs/p7.c
+++ assignments/final_execs/p7.c
@@ -1,60 +1,81 @@
 #include "../../16/thread_107/thread_107.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <curl/curl.h> // include curl for http request 
 #define __APPLE__
 
 int DownloadMediaFile(const char *server, const char *file)
 {
     // TODO http/https request to server/file using c.
+    CURL *curl;
+    CURLcode res;
+    
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
+        /* example.com is redirected, so we tell libcurl to follow redirection */ 
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    
+        /* Perform the request, res will get the return code */ 
+        res = curl_easy_perform(curl);
+        /* Check for errors */ 
+        if(res != CURLE_OK)
+        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+    
+        /* always cleanup */ 
+        curl_easy_cleanup(curl);
+    }
+    
     // TODO count size of downloaded content in bytes. 
-
     return 0;
 }
 
 void *DownloadMediaFileAdapter(void *args)
 {
-    const char *server = ((char **)args)[0];
-    const char *file = ((char **)args)[1];
-    int *filesSizeSumPointer = ((int **)args)[2];
-    Semaphore filesSizeSumLock = *(((Semaphore **)args)[3]);
-    Semaphore maxConnections = *(((Semaphore **)args)[4]);
-    Semaphore filesCounterLock = *(((Semaphore **)args)[5]);
+    const char *debugName = ((char **)args)[0]; 
+    const char *server = ((char **)args)[1];
+    const char *file = ((char **)args)[2];
 
-    printf("%s %s\n", server, file);
+    int *sum = ((int **)args)[3];
+    Semaphore calculateSum = *(((Semaphore **)args)[4]);
+    Semaphore maxConnections = *(((Semaphore **)args)[5]);
+    Semaphore done = *(((Semaphore **)args)[6]);
+
     SemaphoreWait(maxConnections);
-
     int fileSize = DownloadMediaFile(server, file);
-
-    SemaphoreWait(filesSizeSumLock);
-    *filesSizeSumPointer += fileSize;
-    SemaphoreSignal(filesSizeSumLock);
-
-    SemaphoreSignal(filesCounterLock);
     SemaphoreSignal(maxConnections);
+
+    SemaphoreWait(calculateSum);
+    *sum += fileSize;
+    SemaphoreSignal(calculateSum);
+
+    SemaphoreSignal(done);
 }
 
 int DownloadMediaLibrary(const char *server, const char *files[], int numFiles)
 {
-    int filesSizeSum = 0;
-    Semaphore maxConnectionsLock = SemaphoreNew("maxConnectionsLock", 12);
-    Semaphore filesCounterLock = SemaphoreNew("filesLock", 0);
-    Semaphore filesSizeSumLock = SemaphoreNew("filesSizeSumLock", 1);
+    int sum = 0;
+    Semaphore maxConnections = SemaphoreNew("maxConnections", 12);
+    Semaphore done = SemaphoreNew("done", 0);
+    Semaphore calculateSum = SemaphoreNew("calculateSum", 1);
 
     for (int i = 0; i < numFiles; i++)
     {
-        ThreadNew("DownloadMediaFileAdapter", DownloadMediaFileAdapter, 5, &server, &(files[i]),
-                                                                            &filesSizeSum, &filesSizeSumLock,
-                                                                            &maxConnectionsLock, &filesCounterLock);
+        char str[30];
+        sprintf(str, "DownloadMediaFileAdapter_%d", i + 1);
+        ThreadNew(str, DownloadMediaFileAdapter, 6, server, files[i], &sum,
+                                                        &calculateSum, &maxConnections, &done);
     }
 
     RunAllThreads();
 
     for (int i = 0; i < numFiles; i++)
     {
-        SemaphoreWait(filesCounterLock);
+        SemaphoreWait(done);
     }
 
-    return filesSizeSum;
+    return sum;
 }
 
 int main(int argc, char **argv)
@@ -67,7 +88,8 @@ int main(int argc, char **argv)
         "file2"
     };
 
-    int files_size_sum = DownloadMediaFile(server, files); 
-    fprintf("files size sum: %d", files_size_sum);
-    return 0;
+    int files_size_sum = DownloadMediaLibrary(server, files, 2); 
+    fprintf(stdout, "files size sum: %d\n", files_size_sum);
+
+    exit(EXIT_SUCCESS);
 }
