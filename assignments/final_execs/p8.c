3f2c86e96ad1e386b8dff4b2563a255175d1c823
Summerlve <dzxx0563zh@126.com>
2019-04-03 22:10:16 +0800

    p8

--- assignments/final_execs/p8.c
+++ assignments/final_execs/p8.c
@@ -1,12 +1,49 @@
+#include "../../16/thread_107/thread_107.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#define __APPLE__
 
 typedef struct {
     enum {Boolean, Integer, String, Symbol, Empty, List} type;
     char value[8];
 } Expression;
 
+Expression *evaluateExpression(Expression *expr)
+{
+    Expression e = {
+        .type = Boolean,
+        .value = "false"
+    };
+
+    Expression *ep = (Expression *)malloc(sizeof(Expression));
+    memcpy(ep, &e, sizeof(Expression));
+    
+    return ep;
+}
+
+void *evaluateExpressionAdapter(void *args)
+{
+    const char *debugName = NULL;
+    Expression *e = NULL;
+
+    return evaluateExpression(e);
+}
+
+Expression *evaluateConcurrentAnd(Expression *exprs[], int n)
+{
+    bool result = false;
+
+    for (int i = 0; i < n; i++) 
+    {
+        ThreadNew("evaluateExpression", evaluateExpressionAdapter, 1, exprs[i]);
+    }
+    return NULL;
+}
+
 int main(int argc, char **argv)
 {
+    InitThreadPackage(false);
     return 0; 
 }
